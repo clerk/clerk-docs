8155f4b4b298094980e203de879cf38f414c4e86
James Perkins <james@clerk.dev>
2023-02-06 16:13:55 -0500

    Adding Table component:
    - No title restrictions
    - Unlimited rows
    - Overflow scrolls

---
+++ src/components/Table.tsx
@@ -0,0 +1,47 @@
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableContainer,
+  } from '@chakra-ui/react'
+
+
+  type TableRow = {
+    cells: Array<string>
+  };
+
+export const Tables = ({headings, rows}: {headings:Array<string>, rows: TableRow[]  }) => {
+    return (
+    <TableContainer>
+  <Table variant='simple'>
+    <Thead>
+      <Tr>
+      {headings?.map((heading, index) => (
+                  <Th
+                    key={`th-${index}`}
+                  >
+                    {heading}
+                  </Th>
+                ))}
+      </Tr>
+    </Thead>
+    <Tbody>
+    {rows.map((row, index) => (
+                <Tr key={`row-${index}`}>
+                  {row.cells.map((cell, idx) => (
+                    <Td
+                      key={`cell-${index}-${idx}`}
+                    >
+                      {cell}
+                    </Td>
+                  ))}
+                </Tr>
+              ))}
+    </Tbody>
+  </Table>
+</TableContainer>
+)
+}

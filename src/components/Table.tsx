0b0e537f274d74ebf4d0fca59242bbeabf5ff575
James Perkins <jamesperkins@hey.com>
2023-02-07 11:38:15 -0500

    Remove logos Update: auth with callback clerk loaded cler loading multisess sign in sign up sign in button

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,50 +1,55 @@
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
-    TableContainer,
-  } from '@chakra-ui/react'
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
 
+type TableRow = {
+  cells: Array<string>;
+};
 
-  type TableRow = {
-    cells: Array<string>
-  };
-
-export const Tables = ({headings, rows}: {headings:Array<string>, rows: TableRow[]  }) => {
-    return (
+export const Tables = ({
+  headings,
+  rows,
+}: {
+  headings: Array<string>;
+  rows: TableRow[];
+}) => {
+  return (
     <TableContainer>
-  <Table variant='simple'>
-    <Thead>
-      <Tr>
-      {headings?.map((heading, index) => (
-                  <Th
-                    key={`th-${index}`}
-                  >
-                    {heading}
-                  </Th>
-                ))}
-      </Tr>
-    </Thead>
-    <Tbody>
-    {rows.map((row, index) => (
-                <Tr key={`row-${index}`}>
-                  {row.cells.map((cell, idx) => (
-                    <Td
-                      key={`cell-${index}-${idx}`}
-                    >
-                      
-                      {cell}
-                      
-                    </Td>
-                  ))}
-                </Tr>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            {headings?.map((heading, index) => (
+              <Th key={`th-${index}`}>{heading}</Th>
+            ))}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {rows.map((row, index) => (
+            <Tr key={`row-${index}`}>
+              {row.cells.map((cell, idx) => {
+                const result = cell.split(/\r?\n/);
+                if (result.length > 1) {
+                    return (
+                        <Td key={`cell-${index}-${idx}`}>
+                            {result.map((line, i) => (
+                                <span key={`line-${i}`}>{line}<br /></span>
+                            ))}
+                        </Td>
+                    );
+                }
                 
-              ))}
-    </Tbody>
-  </Table>
-</TableContainer>
-)
-}
+                return <Td key={`cell-${index}-${idx}`}>{cell}</Td>;
+              })}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};

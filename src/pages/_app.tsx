471518f56ff779fc046324c7d9e558a5b52bf59d
James Perkins <jamesperkins@hey.com>
2023-02-10 10:28:57 -0500

    Updating to TSX only.
    Adding more details to the Next.js starter.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+
 export default function App({ Component, pageProps }: AppProps) {
   return <ChakraProvider><Component {...pageProps} /></ChakraProvider>
 }

d4b5b8e425e6462fc61b50e43eb648e2ad5f1d99
James Perkins <jamesperkins@hey.com>
2023-02-22 14:16:27 -0500

    Add OG images and auto generated og images.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+  themeConfig: './theme.config.tsx',
 })
  
 module.exports = withNextra({

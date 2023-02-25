92a164dfaded81779c5f4446b814e2abe5cd0b05
James Perkins <jamesperkins@hey.com>
2023-02-25 14:07:59 -0500

    Add sitemap and tracking.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script defer data-domain="beta-docs.clerk.dev" src="https://plausible.io/js/script.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />

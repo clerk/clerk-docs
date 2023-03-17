4d97f158c2e9f4dda3c50692081b893eb9849110
James Perkins <jamesperkins@hey.com>
2023-03-17 08:46:45 -0400

    adding changes.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <ChakraProvider><Component {...pageProps} /></ChakraProvider>
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }

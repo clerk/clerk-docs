2d55fc1feee596e510298661c0f896d090ad02f9
James Perkins <jamesperkins@hey.com>
2023-03-18 17:38:31 -0400

    Remove Chakra in favor of tailwind.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,13 +1,3 @@
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  TableContainer,
-} from "@chakra-ui/react";
-
 type TableRow = {
   cells: Array<string>;
 };
@@ -20,36 +10,57 @@ export const Tables = ({
   rows: TableRow[];
 }) => {
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
+    <div className="flex flex-col overflow-x-auto">
+  <div className="sm:-mx-6 lg:-mx-8">
+    <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+      <div className="overflow-x-auto">
+      <table className="min-w-full text-left text-sm">
+        <thead className="border-b font-medium dark:border-neutral-500">
+          <tr>
             {headings?.map((heading, index) => (
-              <Th key={`th-${index}`}>{heading}</Th>
+              <th scope="col" className="px-6 py-4" key={`th-${index}`}>
+                {heading}
+              </th>
             ))}
-          </Tr>
-        </Thead>
-        <Tbody>
+          </tr>
+        </thead>
+        <tbody>
           {rows.map((row, index) => (
-            <Tr key={`row-${index}`}>
+            <tr className="border-b dark:border-neutral-500" key={`row-${index}`}>
               {row.cells.map((cell, idx) => {
                 const result = cell.split(/\r?\n/);
                 if (result.length > 1) {
-                    return (
-                        <Td key={`cell-${index}-${idx}`}>
-                            {result.map((line, i) => (
-                                <span key={`line-${i}`}>{line}<br /></span>
-                            ))}
-                        </Td>
-                    );
+                  return (
+                    <td
+                      className="whitespace-nowrap px-6 py-4"
+                      key={`cell-${index}-${idx}`}
+                    >
+                      {result.map((line, i) => (
+                        <span className="my-2 " key={`line-${i}`}>
+                          {line}
+                          <br />
+                        </span>
+                      ))}
+                    </td>
+                  );
                 }
-                
-                return <Td key={`cell-${index}-${idx}`}>{cell}</Td>;
+
+                return (
+                  <td
+                    className="whitespace-nowrap px-6 py-4"
+                    key={`cell-${index}-${idx}`}
+                  >
+                    {cell}
+                  </td>
+                );
               })}
-            </Tr>
+            </tr>
           ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+        </tbody>
+      </table>
+    </div>
+    </div>
+  </div>
+</div>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-
+import "../styles/globals.css"
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
       <Component {...pageProps} />
-    </ChakraProvider>
   );
 }

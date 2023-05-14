be154cd82e4ce0fa6698a90d25eaee728ee5b226
Corbin Crutchley <git@crutchcorn.dev>
2023-05-14 09:32:49 -0700

    chore: format with prettier

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,15 +1,19 @@
-
 import Image from "next/image";
 
-export const Images = ({src, alt,width,height} : {src: string, alt: string, width: number, height: number}) => {
-    return (
-        <div className="container flex mx-auto justify-center">
-        <Image
-            src={src}
-            width={width}
-            height={height}
-            alt={alt}
-        />
-        </div>
-    );
+export const Images = ({
+  src,
+  alt,
+  width,
+  height,
+}: {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+}) => {
+  return (
+    <div className="container flex mx-auto justify-center">
+      <Image src={src} width={width} height={height} alt={alt} />
+    </div>
+  );
 };

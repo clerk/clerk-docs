148be5931c09c0e1c17fb3037c2d783e679a89f6
Corbin Crutchley <git@crutchcorn.dev>
2023-05-14 09:41:15 -0700

    chore: fix sitemap and robots.txt for dev mode

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next-sitemap').IConfig} */
+/** @type {import("next-sitemap").IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://beta-docs.clerk.dev/',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  }
+  siteUrl: process.env.SITE_URL || "https://beta-docs.clerk.dev/",
+  generateRobotsTxt: true,
+  // TODO: REMOVE THIS BEFORE DEPLOYMENT
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", disallow: "/" }],
+  },
+};

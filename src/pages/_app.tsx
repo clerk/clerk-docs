817ae03ccbc5003d1d2e4dad921222944d5b52fd
Liliana Weimer <lilianaweimer@Lilianas-MacBook-Pro.local>
2023-05-31 10:19:49 -0600

    chore: homepage redesign
    *chore: add new fonts
    *chore: fix Hero image not changing colors on system theme
    *chore: add new default font to components
    *chore: add animation for arrows on hover

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,21 @@ module.exports = {
     "./src/pages/**/*.{md,mdx}",
     "./theme.config.tsx",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        figtree: ["var(--font-figtree)"],
+      },
+      keyframes: {
+        slide: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(4px)" },
+        },
+      },
+      animation: {
+        "move-arrow": "slide 0.2s ease-out 0s 1 normal forwards",
+      },
+    },
+  },
 };

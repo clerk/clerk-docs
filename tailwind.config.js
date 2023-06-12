4c7604908bce541e1c514121a831e445080e2764
lweimerclerk <133695690+lweimerclerk@users.noreply.github.com>
2023-06-12 14:33:11 -0600

    fix: change arrow animation to reverse correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,6 @@ module.exports = {
         inter: ["var(--font-inter)"],
         figtree: ["var(--font-figtree)"],
       },
-      keyframes: {
-        slide: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(4px)" },
-        },
-      },
-      animation: {
-        "move-arrow": "slide 0.2s ease-out 0s 1 normal forwards",
-      },
       colors: {
         "clerk-purple": "#6C47FF",
       },

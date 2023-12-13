b76d7ac3505a9dc411f2d5dbd0c5ca9a0c74c63a
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2023-12-13 15:26:28 -0500

    setting up frontmatter plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,44 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
-	},
-	"extends": ["eslint:recommended", "plugin:mdx/recommended"],
-	"overrides": [
-		{
-			"env": {
-				"node": true
-			},
-			"files": [
-				".eslintrc.{js,cjs}"
-			],
-			"parserOptions": {
-				"sourceType": "script"
-			}
-		}
-	],
-	"parserOptions": {
-		"ecmaVersion": "latest"
-	},
-	"rules": {
-		"indent": [
-			"off",
-			"tab"
-		],
-		"linebreak-style": [
-			"off",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:mdx/recommended",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    "mdx/code-blocks": true,
+  },
+  plugins: ["prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["off", "tab"],
+    "prefer-arrow-callback": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: `es5`,
+        semi: true,
+        singleQuote: false,
+        printWidth: 80,
+        endOfLine: "auto",
+      },
+    ],
+  },
 };

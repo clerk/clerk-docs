b7df591ec0c7171a681242c5453f7267b2c8b4f7
Brad Cornes <hello@bradley.dev>
2024-05-31 12:33:34 +0100

    wip

--- check.mjs
+++ check.mjs
@@ -1,31 +1,28 @@
-import fs from "node:fs/promises";
-import { compile as _compile } from "@mdx-js/mdx";
-import glob from "fast-glob";
-import * as prettier from "prettier";
+import fs from 'node:fs/promises'
+import { compile as _compile } from '@mdx-js/mdx'
+import glob from 'fast-glob'
+import * as prettier from 'prettier'
 
-const files = await glob("docs/**/*.mdx");
+const files = await glob('docs/**/*.mdx')
 
 function normalizeNewline(str) {
-  return str.replace(/\\r\\n/g, "\\n");
+  return str.replace(/\\r\\n/g, '\\n')
 }
 
 function removeFrontmatter(source) {
-  return source.replace(
-    /^---(?:\r?\n|\r)(?:([\s\S]*?)(?:\r?\n|\r))?---(?:\r?\n|\r|$)/,
-    "",
-  );
+  return source.replace(/^---(?:\r?\n|\r)(?:([\s\S]*?)(?:\r?\n|\r))?---(?:\r?\n|\r|$)/, '')
 }
 
 async function compile(source) {
-  return normalizeNewline(String(await _compile(removeFrontmatter(source))));
+  return normalizeNewline(String(await _compile(removeFrontmatter(source))))
 }
 
 for (let filename of files) {
-  let source = await fs.readFile(filename, "utf-8");
-  let options = await prettier.resolveConfig(filename);
-  let formatted = await prettier.format(source, options);
+  let source = await fs.readFile(filename, 'utf-8')
+  let options = await prettier.resolveConfig(filename)
+  let formatted = await prettier.format(source, { ...options, mdxFormatCodeBlocks: false })
 
   if ((await compile(source)) !== (await compile(formatted))) {
-    console.log("Compiled output does not match:", filename);
+    console.log('Compiled output does not match:', filename)
   }
 }

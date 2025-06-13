860ad4ac3c403118797200170051a3420b9c66d6
Nick Wylynko <nickwylynko@clerk.dev>
2025-06-14 00:36:13 +0800

    attempt using native Request, Response api

--- api/redirect.ts
+++ api/redirect.ts
@@ -1,13 +1,20 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+// import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default function handler(request: VercelRequest, response: VercelResponse) {
+export default function handler(request: Request) {
   const branchUrl = process.env.VERCEL_BRANCH_URL
 
   if (!branchUrl) {
-    return response.status(404).json({ error: 'Not found' })
+    // return response.status(404).json({ error: 'Not found' })
+    return new Response('Not found', { status: 404 })
   }
 
   const slug = branchUrl.replace('clerk-docs-git-', '').replace('.clerkstage.dev', '')
 
-  return response.redirect(308, `https://clerk.com/docs/pr/${slug}`)
+  // return response.redirect(308, `https://clerk.com/docs/pr/${slug}`)
+  return new Response(null, {
+    status: 308,
+    headers: {
+      Location: `https://clerk.com/docs/pr/${slug}`,
+    },
+  })
 }

d6f439ac64b14057a5287bfc099e4e349c6a4185
Nick Wylynko <nickwylynko@clerk.dev>
2025-06-14 00:22:18 +0800

    redirect the user as we need

--- api/redirect.ts
+++ api/redirect.ts
@@ -1,10 +1,13 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(request: VercelRequest, response: VercelResponse) {
-  console.log(process.env)
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  })
+  const branchUrl = process.env.VERCEL_BRANCH_URL
+
+  if (!branchUrl) {
+    return response.status(404).json({ error: 'Not found' })
+  }
+
+  const slug = branchUrl.replace('clerk-docs-git-', '').replace('.clerkstage.dev', '')
+
+  return response.redirect(308, `https://clerk.com/docs/pr/${slug}`)
 }

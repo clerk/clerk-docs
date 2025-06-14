d738f0272ba8b1e35c33c8dadc20336adab71e25
Nick Wylynko <nickwylynko@clerk.dev>
2025-06-14 20:50:01 +0800

    Remove @vercel/node module

--- api/redirect.ts
+++ api/redirect.ts
@@ -1,21 +1,3 @@
-// import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-// export const config = {
-//   runtime: 'edge',
-// }
-
-// export default function handler(request: VercelRequest, response: VercelResponse) {
-//   const branchUrl = process.env.VERCEL_BRANCH_URL
-
-//   if (!branchUrl) {
-//     return response.status(404).json({ error: 'Not found' })
-//   }
-
-//   const slug = branchUrl.replace('clerk-docs-git-', '').replace('.clerkstage.dev', '')
-
-//   return response.redirect(308, `https://clerk.com/docs/pr/${slug}`)
-// }
-
 export const config = {
   runtime: 'edge',
 }

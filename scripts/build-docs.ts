cc49496bc80ddc2f5257f185a4d377ccb3eba6a3
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2025-07-03 11:52:19 -0400

    Use typedoc for OrganizationCustomRoleKey

--- scripts/build-docs.ts
+++ scripts/build-docs.ts
@@ -145,7 +145,6 @@ async function main() {
       typedoc: {
         'types/active-session-resource.mdx': ['link-hash-not-found'],
         'types/pending-session-resource.mdx': ['link-hash-not-found'],
-        'types/organization-custom-role-key.mdx': ['link-doc-not-found'],
       },
       partials: {},
     },

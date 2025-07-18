21e573b856ec19256379595973ef2b8ae4fe8ebe
Nick Wylynko <nickwylynko@clerk.dev>
2025-07-18 16:27:45 -0400

    fix  up link script

--- scripts/link-typedoc.ts
+++ scripts/link-typedoc.ts
@@ -1,14 +1,23 @@
 // eg npm run typedoc:link ../javascript/.typedoc/docs
 
+import { existsSync } from 'node:fs'
+import { rm } from 'node:fs/promises'
 import symlinkDir from 'symlink-dir'
 
 const TYPEDOC_DIR = './clerk-typedoc'
 
-const typedocFolderLocation = process.argv[2]
+const main = async () => {
+  const typedocFolderLocation = process.argv[2]
 
-if (!typedocFolderLocation) {
-  console.error('No typedoc folder location provided')
-  process.exit(1)
+  if (!typedocFolderLocation) {
+    throw new Error('No typedoc folder location provided')
+  }
+
+  if (existsSync(TYPEDOC_DIR)) {
+    await rm(TYPEDOC_DIR, { recursive: true })
+  }
+
+  await symlinkDir(typedocFolderLocation, TYPEDOC_DIR)
 }
 
-await symlinkDir(typedocFolderLocation, TYPEDOC_DIR)
+main()

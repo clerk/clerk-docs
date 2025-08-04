f491c383a58afe1e3fd789cccda3600c906e26b5
Nick Wylynko <nickwylynko@clerk.dev>
2025-08-04 21:36:47 +0800

    missing vars

--- scripts/check-typedoc-edits.js
+++ scripts/check-typedoc-edits.js
@@ -1,4 +1,6 @@
 module.exports = async ({ github, context, core }) => {
+  const { owner, repo, number } = context.issue
+
   // Check if we've already commented on this PR
   const comments = await github.rest.issues.listComments({
     owner,

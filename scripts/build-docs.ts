e8f6208d2b49a89d73f4bd95577f870e423c078d
Alex Carpenter <alex.carpenter@clerk.dev>
2025-08-11 14:21:10 -0400

    Update build-docs.ts

--- scripts/build-docs.ts
+++ scripts/build-docs.ts
@@ -171,6 +171,7 @@ async function main() {
         'upgrade-guides/core-2/react.mdx': ['doc-not-in-manifest'],
         'upgrade-guides/core-2/remix.mdx': ['doc-not-in-manifest'],
         'upgrade-guides/core-2/javascript.mdx': ['doc-not-in-manifest'],
+        'quickstarts/nextjs/shadcn.mdx': ['doc-not-in-manifest'],
       },
       typedoc: {
         'types/active-session-resource.mdx': ['link-hash-not-found'],

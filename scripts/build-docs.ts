0cfe90f30e47225b67c8ac2cdf128795c8a895fe
Nick Wylynko <nickwylynko@clerk.dev>
2025-09-18 22:19:22 +0800

    Overwrite the existing dist if it exists

--- scripts/build-docs.ts
+++ scripts/build-docs.ts
@@ -1291,7 +1291,7 @@ ${yaml.stringify({
     // While in dev, we just want to symlink the new dist to the dist folder
     // This removes the issue that fs.cp can't replace a folder
     // We don't need to worry about the public folder because in dev clerk/clerk just looks in the original public folder
-    await symlinkDir(config.distTempPath, config.distFinalPath)
+    await symlinkDir(config.distTempPath, config.distFinalPath, { overwrite: true })
   } else if (process.env.VERCEL === '1') {
     // In vercel ci the temp dir and the final dir will be on separate partitions so fs.rename() will fail
     await fs.cp(config.distTempPath, config.distFinalPath, { recursive: true })
